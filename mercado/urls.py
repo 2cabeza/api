914d8cc131774072b71fa82a85f0fa905546cee9
2cabeza <72514789+2cabeza@users.noreply.github.com>
2023-11-07 00:37:30 -0300

    first commit

--- mercado/urls.py
+++ mercado/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from app_mercado import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 
-urlpatterns = [
+urlpatterns += [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    staticfiles_urlpatterns()
 ]
 
