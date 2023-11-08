11f05763c705c8f1d727618fb0c33a78c1359c9a
2cabeza <72514789+2cabeza@users.noreply.github.com>
2023-11-08 16:59:45 -0300

    settings

--- mercado/settings.py
+++ mercado/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.CommonMiddleware.whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mercado.urls'
@@ -131,8 +132,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 #STATICFILES_DIRS = (os.path.join(BASE_DIR, 'render/static'),)
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static/admin")
+STATICFILES_DIRS = os.path.join(BASE_DIR, "/static")
+
+STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 
 
